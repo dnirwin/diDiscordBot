bcc1d8a491ab72cd936e9e52886e007a571da7f2
Dylan <sboarder0111@gmail.com>
2021-10-25 14:30:45 -0400

    renamed musicCommands.py to music.py for simplicity

--- cogs/music.py
+++ cogs/music.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class MusicCommands(commands.Cog):
+class Music(commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -10,4 +10,4 @@ class MusicCommands(commands.Cog):
 
 
 def setup(client):
-    client.add_cog(MusicCommands(client))
+    client.add_cog(Music(client))
