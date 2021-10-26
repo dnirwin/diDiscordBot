9d4a94a8bd189b2fcb8c0a253a3957aa46dba9bd
Dylan <sboarder0111@gmail.com>
2021-10-26 11:39:49 -0400

    music cog runs but still doesnt work

--- cogs/music.py
+++ cogs/music.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
+from discord import FFmpegPCMAudio
 import youtube_dl #pip install youtube_dl
+
 
 class Music(commands.Cog):
 
@@ -10,7 +12,7 @@ class Music(commands.Cog):
     @commands.command()
     async def join(self, ctx):
         if ctx.author.voice is None:
-            await ctx.send('You\'re not in a voice channel, cannot connect.')
+            await ctx.send('You\'re not in a voice channel, I cannot connect.')
         voice_channel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await voice_channel.connect()
