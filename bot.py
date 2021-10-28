7bdc356c7acfd2e13efc11f4889ad5f85373c8c7
Dylan <sboarder0111@gmail.com>
2021-10-28 19:16:10 -0400

    Added a json for token to be placed in

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import json
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
@@ -30,4 +31,7 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('Enter Token Here')
+f = open('config.json')
+token = json.load(f)
+
+client.run(token['token'])
