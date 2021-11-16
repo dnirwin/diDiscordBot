6c8a69b7d9c81bec6b15db012e5111f30cee2750
Dylan <sboarder0111@gmail.com>
2021-11-16 15:40:01 -0500

    Both youtube streaming and local file play work, made them only one cog, titled: sounds

--- cogs/sounds.py
+++ cogs/sounds.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio # pip install asyncio
 import discord
 import youtube_dl # pip install youtube_dl
 from discord.ext import commands
@@ -21,6 +21,29 @@ ffmpeg_options = {
     'options': '-vn'
 }
 
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
 class Music(commands.Cog):
 
     def __init__(self, client):
@@ -30,15 +53,12 @@ class Music(commands.Cog):
     async def join(self, ctx):
         if ctx.author.voice is None:
             await ctx.send('You\'re not in a voice channel, I cannot connect.')
+
         voice_channel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await voice_channel.connect()
         else:
             await ctx.voice_client.move_to(voice_channel)
-
-    @commands.command()
-    async def leave(self, ctx):
-        await ctx.voice_client.disconnect()
 
     @commands.command()
     async def play(self, ctx, *, url):
@@ -51,15 +71,29 @@ class Music(commands.Cog):
             source = await discord.FFmpegOpusAudio.from_probe(url2, **ffmpeg_options)
             vc.play(source)
 
+        player = await YTDLSource.from_url(url, loop=self.client.loop, stream=True)
+        await ctx.send(f'Now playing: {player.title}')
+
+    @commands.command(aliases=['sb'])
+    async def soundboard(self, ctx, *, query):
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        vc = ctx.voice_client
+        vc.play(source)
+        await ctx.send(f'Now playing: {query}')
+
     @commands.command()
     async def pause(self, ctx, url):
         await ctx.voice_client.pause()
         await ctx.send('Music Paused')
 
-    @commands.command()
+    @commands.command(aliases=['unpause'])
     async def resume(self, ctx, url):
         await ctx.voice_client.resume()
         await ctx.send('Music Resumed')
+
+    @commands.command(aliases=['stop', 'disconnect'])
+    async def leave(self, ctx):
+        await ctx.voice_client.disconnect()
 
 def setup(client):
     client.add_cog(Music(client))
