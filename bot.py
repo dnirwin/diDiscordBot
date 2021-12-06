5021f041790bfb9decf5f5230e52740237376ec8
Dylan <sboarder0111@gmail.com>
2021-12-06 16:21:29 -0500

    Added CustomHelpCommand class in bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,26 @@ import os
 import json
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+class CustomHelpCommand(commands.HelpCommand):
+
+    def __init__(self):
+        super().__init__()
+
+    async def send_bot_help(self, mapping):
+        for cog in mapping:
+            await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in mapping[cog]]}')
+
+    async def send_cog_help(self, cog):
+        await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in cog.get_commands()]}')
+
+    async def send_group_help(self, group):
+        await self.get_destination().send(f'{cog.qualified_name}: {[command.name for index, command in enumerate(group.commands)]}')
+
+    async def send_command_help(self, command):
+        await self.get_destination().send(command.name)
+
+
+client = commands.Bot(command_prefix = '.', help_command = CustomHelpCommand())
 
 @client.event
 async def on_ready():
